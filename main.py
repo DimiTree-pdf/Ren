811079783858e3e8f16af010e6dce449bf2f5a9c
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-26 16:43:23 -0400

    Updated main.py
    testing dynamically deleting html elements

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from browser import document, prompt, html, alert
 cont = document["container"]
-def clearchildren(obj):
-    obj.clear()
+newdiv - html.DIV(id = "newdiv")
+def clearchildren():
+    del cont[newdiv]
 button1 = html.BUTTON("Clear me! (hopefully)")
-cont <= button1
-cont[button1].bind("click", clearchildren(cont))
+newdiv <= button1
+newdiv[button1].bind("click", clearchildren(cont))
+cont <= newdiv
