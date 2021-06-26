2baf6440b64790f98bfbfda960ff83bdf04eb242
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-26 16:13:50 -0400

    Update main.py
    testing dynamically deleting html elements

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
-from browser import alert, window
-from javascript import this
-import hashlib
-hashes = {
-    "sha-1": hashlib.sha1,
-    "sha-256": hashlib.sha256,
-    "sha-512": hashlib.sha512,
-}
-Vue = window.Vue
-def compute_hash(evt):
-    value = this().input_text
-    if not value:
-        alert(hashlib.sha256.update("You need to enter a value".encode()).hexdigest())
-        return
-    hash_object = hashes[this().algo]()
-    hash_object.update(value.encode())
-    hex_value = hash_object.hexdigest()
-    this().hash_value = hex_value
-def created():
-    for name in hashes:
-        this().algos.append(name)
-    this().algo = next(iter(hashes))
-app = Vue.createApp(
-    {
-        "el": "#app",
-        "created": created,
-        "data": lambda _: {"hash_value": "", "algos": [], "algo": "", "input_text": ""},
-        "methods": {"compute_hash": compute_hash},
-    }
-)
-app.mount("#app")
+from browser import document, prompt, html, alert
+cont = document["container"]
+def clearchildren(obj):
+    obj.clear()
+button1 = html.BUTTON("Clear me! (hopefully)")
+cont <= button1
+cont[button1].bind("click", clearchildren(cont))
