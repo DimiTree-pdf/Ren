20ccc1d548a05cfcd6f379b953de7cb048f37719
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-30 16:51:15 -0400

    Fixed a formatting error

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def clearchildren():
     del cont[newdiv]
 button1 = html.BUTTON("Clear me! (hopefully)")
 newdiv <= button1
-newdiv[button1].bind("click", clearchildren(cont))
+newdiv[button1].bind("click", clearchildren())
 cont <= newdiv
