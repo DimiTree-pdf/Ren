9f0c174cd6ee29fc76f674510be4d44b9b321061
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-30 17:02:57 -0400

    Bugfix where button disappears after binding function to it

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from browser import document, prompt, html, alert
 cont = document["container"]
 newdiv = html.DIV(id = "newdiv")
 def clearchildren():
-    del cont[newdiv]
+    #del cont[newdiv]
+    x = 1
 button1 = html.BUTTON("Clear me! (hopefully)")
 newdiv <= button1
 newdiv[button1].bind("click", clearchildren())
