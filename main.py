f35f4d0e00b0bdac2dc18c3f3681d349f7e5cbae
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-30 18:40:01 -0400

    bugfix

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ cont = document["container"]
 newdiv = html.DIV(id = "newdiv")
 def clearchildren(div):
     del cont[div]
+    testdiv = html.DIV(id = "testdiv")
+    button2 = html.BUTTON("It worked!")
+    button2 <= testdiv
+    testdiv <= cont
 button1 = html.BUTTON("Clear me! (hopefully)")
-button1.bind("click", clearchildren(newdiv))
+#button1.bind("click", clearchildren(newdiv))
 newdiv <= button1
 cont <= newdiv
