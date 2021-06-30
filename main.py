1d6d1ae9993940f6e21940784f59664e8855b209
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-30 19:11:59 -0400

    fixed typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from browser import document, prompt, html, alert
 cont = document["container"]
 newdiv = html.DIV(id = "newdiv")
-def clearchildren():
+def clearchildren(_):
     del cont[newdiv]
     testdiv = html.DIV(id = "testdiv")
     button2 = html.BUTTON("It worked!")
