f599e8d741833e50edbd8a9e22908b5197f5a0f2
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-30 17:29:10 -0400

    Updated main.py
    button should now self-delete

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from browser import document, prompt, html, alert
 cont = document["container"]
 newdiv = html.DIV(id = "newdiv")
 def clearchildren():
-    #del cont[newdiv]
+    del cont[newdiv]
     x = 1
 button1 = html.BUTTON("Clear me! (hopefully)")
 button1.bind("click", clearchildren())
