4f38ce8d0cbb1945a3644e65a0d271715e1fb5ea
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-30 16:16:52 -0400

    Updated main.py to connect the delete function to the button

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from browser import document, prompt, html, alert
 cont = document["container"]
 newdiv = html.DIV(id = "newdiv")
-#def clearchildren():
-    #del cont[newdiv]
+def clearchildren():
+    del cont[newdiv]
 button1 = html.BUTTON("Clear me! (hopefully)")
 newdiv <= button1
-#newdiv[button1].bind("click", clearchildren(cont))
+newdiv[button1].bind("click", clearchildren(cont))
 cont <= newdiv
