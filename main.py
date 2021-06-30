e10db1b3ba1578954e76a610bb8bc24f97219cde
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-06-30 17:38:58 -0400

    bugfix

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from browser import document, prompt, html, alert
 cont = document["container"]
 newdiv = html.DIV(id = "newdiv")
-def clearchildren():
-    del cont[newdiv]
-    x = 1
+def clearchildren(div):
+    del cont[div]
 button1 = html.BUTTON("Clear me! (hopefully)")
-button1.bind("click", clearchildren())
+button1.bind("click", clearchildren(newdiv))
 newdiv <= button1
 cont <= newdiv
