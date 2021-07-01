c057aab00a62a5d55028fa6931088f273f04af77
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-07-01 13:22:36 -0400

    Updated main.py
    should now display buttons correctly (forgot bindings)

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ signupbool = True
 signedin = False
 cont = document["container"]
 newdiv = html.DIV(id = "newdiv")
-signin = html.BUTTON("Sign Up Page")
-login = html.BUTTON("Log In Page")
+signupbutton = html.BUTTON("Sign Up Page")
+loginbutton = html.BUTTON("Log In Page")
 def switchlogin(_):
     signupbool = False
-def switchsignin(_):
+def switchsignup(_):
     signupbool = True
 def signup(_):
     username = cont["signupdiv"]["signupusername"].value
@@ -27,13 +27,30 @@ def signup(_):
     #we don't have a working database yet so we'll just leave this part blank for now
     #TODO: store account information in db 
     signedin = True
-lastsignbool = False
+lastsignbool = True
+signupbutton.BIND("click", switchsignup)
+loginbutton.BIND("click", switchlogin) 
+newdiv <= signupbutton
+newdiv <= loginbutton
+cont <= newdiv
+signupdiv = html.DIV(id = "signupdiv")
+signupusername = html.INPUT(type="text", name="username", value="Username")
+signuppassword = html.INPUT(type="text", name="password", value="Password")
+signupconfirm = html.INPUT(type="text", name="confirm", value="Confirm Password")
+signupdiv <=  signupusername + html.BR()
+signupdiv <=  signuppassword + html.BR()
+signupdiv <=  signupconfirm + html.BR()
+submitsignup = html.BUTTON("Sign Up")
+submitsignup.BIND("click", signup)
+signupdiv <= submitsignup
+newdiv <= signupdiv
 while not signedin:
     if lastsignbool != signupbool:
         #this means that the page loaded has changed. 
         #therefore, we must redraw the page
         if signupbool:
             #we have to redraw the sign-up page
+            del cont[logindiv]
             signupdiv = html.DIV(id = "signupdiv")
             signupusername = html.INPUT(type="text", name="username", value="Username")
             signuppassword = html.INPUT(type="text", name="password", value="Password")
@@ -42,5 +59,7 @@ while not signedin:
             signupdiv <=  signuppassword + html.BR()
             signupdiv <=  signupconfirm + html.BR()
             submitsignup = html.BUTTON("Sign Up")
-                                  
+            submitsignup.BIND("click", signup)
+            signupdiv <= submitsignup
+            newdiv <= signupdiv
             
