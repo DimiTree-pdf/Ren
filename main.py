a6f9e5c3fbcdb7378a4b81f472ff94149141af89
Portal <80373824+DimiTree-pdf@users.noreply.github.com>
2021-07-14 08:20:41 -0400

    bugfixing

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def switchlogin(_):
 def switchsignup(_):
     signupbool = True
     #_.stopPropagation()
-def signup(_):
-    username = cont["signupdiv"]["signupusername"].value
-    password = cont["signupdiv"]["signuppassword"].value
-    confirm = cont["signupdiv"]["signupconfirm"].value
-    if (not username | not password | not confirm) | (username == "Username" | password == "Password" | confirm == "Confirm Password"):
-      alert("You must enter a username and a password")
+#def signup(_):
+    #username = cont["signupdiv"]["signupusername"].value
+    #password = cont["signupdiv"]["signuppassword"].value
+    #confirm = cont["signupdiv"]["signupconfirm"].value
+    #if (not username | not password | not confirm) | (username == "Username" | password == "Password" | confirm == "Confirm Password"):
+      #alert("You must enter a username and a password")
     #TODO: check if username is taken
-    if confirm != password:
-       alert("Passwords do not match!")
-    salt = secrets.token_hex(64)
-    hashedpassword = ph.hash(password + salt)
+    #if confirm != password:
+    #   alert("Passwords do not match!")
+    #salt = secrets.token_hex(64)
+    #hashedpassword = ph.hash(password + salt)
     #this part of the code is where we would store the username, salt, and hashed password in a database
     #we don't have a working database yet so we'll just leave this part blank for now
     #TODO: store account information in db 
-    signedin = True
+    #signedin = True
     #_.stopPropagation()
 lastsignbool = True
 loginbutton.bind("click", switchlogin) 
@@ -41,9 +41,9 @@ signupusername = html.INPUT(type="text", name="Username", value="Username")
 signuppassword = html.INPUT(type="text", name="Password", value="Password")
 signupconfirm = html.INPUT(type="text", name="Confirm Password", value="Confirm Password")
 #when mouse clicks outside of inputs, the filler text should be replaced
-def fillsignupinputs(_):
-    for child in signupdiv:
-        alert(child.class_name)
+#def fillsignupinputs(_):
+    #for child in signupdiv:
+        #alert(child.class_name)
 #bind click to inputs so that they delete their text when clicked in
 signupdiv.bind("click", fillsignupinputs)
 signupdiv <=  signupusername + html.BR()
